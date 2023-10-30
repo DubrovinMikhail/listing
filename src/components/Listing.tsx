a8c63da12d0fb832f56e1c437384c312c83047db
dubrovinmikhail <mail@mail.com>
2023-10-30 13:14:20 +0300

    fixed

--- src/components/Listing.tsx
+++ src/components/Listing.tsx
@@ -1,3 +1,6 @@
+interface IMaineImage {
+  url_570xN: string;
+};
 
 export interface IProposal {
   listing_id: number;
@@ -6,9 +9,7 @@ export interface IProposal {
   price: string;
   quantity: number;
   title: string;
-  MainImage: {
-    url_570xN: string;
-  };
+  MainImage: IMaineImage;
 };
 
 type IProposalListProps = {
@@ -16,8 +17,7 @@ type IProposalListProps = {
 }
 
 export const Listing = ({ items }:IProposalListProps) => {
-
-  console.log(items)
+  console.log(items[0].MainImage.url_570xN)
 
   return (
     <div className="item-list">
@@ -25,17 +25,25 @@ export const Listing = ({ items }:IProposalListProps) => {
         <div className="item" key={item.listing_id}>
           <div className="item-image">
             <a href={item.url}>
-              {}
-              <img src={item.MainImage.url_570xN} alt={item.title}/>
+            {item.MainImage && item.MainImage.url_570xN &&(
+              <img src={item.MainImage.url_570xN} alt={item.title} /> )}
             </a>
           </div>
           <div className="item-details">
-            <p className="item-title">{item.title}</p>
-            <p className="item-price">{item.currency_code}{item.price}</p>
-            <p className="item-quantity level-medium">{item.quantity}</p>
+            <p className="item-title">
+            {item.title && item.title.length > 50 ? `${item.title.slice(0, 50)}…` : item.title}
+            </p>
+            <p className="item-price">
+            {item.currency_code === 'USD' ? '$' :
+               item.currency_code === 'EUR' ? '€' :
+               `${item.currency_code} `}
+              {item.price}
+              </p>
+            <p className={`item-quantity ${item.quantity <= 10 ? 'level-low' : item.quantity <= 20 ? 'level-medium' : 'level-high'}`}>{item.quantity}</p>
           </div>
         </div>
       ))}
     </div>
   )
 }
+
